9ca5946f77833a3110b0ff4d76f2d737f0b6c51c
liuxun <liuxun@jd.com>
2017-05-22 12:15:02 +0800

    排序算法

--- src/main/java/com/algorithm/study/demo/SortProject.java
+++ src/main/java/com/algorithm/study/demo/SortProject.java
@@ -8,7 +8,7 @@ import java.util.Collections;
  */
 public class SortProject {
     public static void main(String[] args) {
-        int a[] = {70, 69, 100, 87, 89, 90, 2, 1};
+        int a[] = {69, 70, 2, 87,3};
 //        maopaoSort(a);
 //        selectSort(a);
 
@@ -30,9 +30,9 @@ public class SortProject {
      */
     private static void maopaoSort(int score[]){
         boolean flag=true;//数据发生了交换才继续冒泡
-        for (int i = 0; i < score.length -1 && flag; i++){    //最多做n-1趟排序
+        for (int i = 1; i < score.length && flag; i++){    //最多做n-1趟排序
             flag=false;
-            for(int j = 0 ;j < score.length - i - 1; j++){    //对当前无序区间score[0......length-i-1]进行排序(j的范围很关键，这个范围是在逐步缩小的)
+            for(int j = 0 ;j < score.length - i; j++){    //对当前无序区间score[0......length-i-1]进行排序(j的范围很关键，这个范围是在逐步缩小的)
                 if(score[j] > score[j + 1]){    //把小的值交换到后面
                     int temp = score[j];
                     score[j] = score[j + 1];
@@ -40,7 +40,7 @@ public class SortProject {
                     flag=true;//发生了数据交换
                 }
             }
-            System.out.print("第" + (i + 1) + "趟排序结果：");
+            System.out.print("第" + (i) + "趟排序结果：");
             for(int a = 0; a < score.length; a++){
                 System.out.print(score[a] + "\t");
             }
@@ -75,20 +75,19 @@ public class SortProject {
 
     /**
      * 插入排序算法
-     * 插入排序就是把当前待排序的元素插入到一个已经排好序的列表里面
+     * 插入排序就是每一步都将一个待排数据按其大小插入到已经排序的数据中的适当位置，直到全部插入完毕。
      * 性能优于选择排序、冒泡排序
+     * {69, 70, 2, 87}
      */
     private static void insertSort( int ls[]){
-        for (int i=1;i<ls.length;i++){//默认0是已排序的的列表
-            int m=i;//待排序的元素
-            for (int j=i;j>0;j--){
-                if (ls[m]<ls[j-1]){
-                    int temp=ls[m];
-                    ls[m]=ls[j-1];
-                    ls[j-1]=temp;
-                    m=j-1;
-                }
+        for (int i=1;i<ls.length;i++){
+            int key = ls[i];
+            int j = i-1;
+            for(;j>=0&&key<ls[j];j--){
+                ls[j+1]=ls[j];                       //将大于temp的值整体后移一个单位
             }
+            ls[j+1] = key;
+
             System.out.print("第" + (i + 1) + "趟排序结果：");
             for(int a = 0; a < ls.length; a++){
                 System.out.print(ls[a] + "\t");
@@ -125,33 +124,41 @@ public class SortProject {
 
     /**
      * 快速排序
-     * @param list
+     * 基本思想：选择一个基准元素,通常选择第一个元素或者最后一个元素,通过一趟扫描，
+     * 将待排序列分成两部分,一部分比基准元素小,一部分大于等于基准元素,
+     * 此时基准元素在其排好序后的正确位置,然后再用同样的方法递归地排序划分的两部分。
      * @param low
      * @param high
      * @return
      */
-    public static int getMiddle(int[] list, int low, int high) {
-        int tmp = list[low];    //数组的第一个作为中轴
-        while (low < high) {
-            while (low < high && list[high] >= tmp) {
-                high--;
-            }
-            list[low] = list[high];   //比中轴小的记录移到低端
-            while (low < high && list[low] <= tmp) {
-                low++;
-            }
-            list[high] = list[low];   //比中轴大的记录移到高端
-        }
-        list[low] = tmp;              //中轴记录到尾
-        return low;                   //返回中轴的位置
-    }
+    public static void _quickSort(int[] a,int low,int high) {
+        int start = low;
+        int end = high;
+        int key = a[low];
+        while (end > start) {
+            //从后往前比较
+            while (end > start && a[end] >= key)  //如果没有比关键值小的，比较下一个，直到有比关键值小的交换位置，然后又从前往后比较
+                end--;
 
-    public static void _quickSort(int[] list, int low, int high) {
-        if (low < high) {
-            int middle = getMiddle(list, low, high);  //将list数组进行一分为二
-            _quickSort(list, low, middle - 1);        //对低字表进行递归排序
-            _quickSort(list, middle + 1, high);       //对高字表进行递归排序
+            if (a[end] <= key) {
+                int temp = a[end];
+                a[end] = a[start];
+                a[start] = temp;
+            }
+            //从前往后比较
+            while (end > start && a[start] <= key)//如果没有比关键值大的，比较下一个，直到有比关键值大的交换位置
+                start++;
+
+            if (a[start] >= key) {
+                int temp = a[start];
+                a[start] = a[end];
+                a[end] = temp;
+            }
+            //此时第一次循环比较结束，关键值的位置已经确定了。左边的值都比关键值小，右边的值都比关键值大，但是两边的顺序还有可能是不一样的，进行下面的递归调用
         }
+        //递归
+        if (start > low) _quickSort(a, low, start - 1);//左边序列。第一个索引位置到关键值索引-1
+        if (end < high) _quickSort(a, end + 1, high);//右边序列。从关键值索引+1到最后一个
     }
     public static void quick(int[] a2) {
         if (a2.length > 1) {    //查看数组是否为空
