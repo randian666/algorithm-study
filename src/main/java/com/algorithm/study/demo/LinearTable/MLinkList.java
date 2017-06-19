bc2da88c8e24cc0ac103a277db75aa050a3f1e24
liuxun <liuxun@jd.com>
2017-06-19 12:27:46 +0800

    单链表实现

---
+++ src/main/java/com/algorithm/study/demo/LinearTable/MLinkList.java
@@ -0,0 +1,143 @@
+package com.algorithm.study.demo.LinearTable;
+
+/**
+ * Created by liuxun on 2017/6/16.
+ */
+public class MLinkList<E> {
+    // 定义一个内部类Node，代表链表的节点
+    private class Node {
+        private E data;// 保存数据
+        private Node next;// 指向下个节点的引用
+
+        // 无参构造器
+        public Node() {
+        }
+        // 初始化全部属性的构造器
+        public Node(E data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+    private Node data;// 保存头结点
+//    private Node last;// 保存尾结点
+    private int size=0;// 保存已含有的节点数
+
+    public MLinkList(){
+        this.data=null;//初始化一个空的头结点
+    }
+
+    /**
+     * 添加一个头结点
+     * @param element
+     */
+    public void addFirst(E element){
+        if (data==null){
+            data=new Node(element,null);
+        }else{
+            Node temp=new Node(element,null);
+            temp.next=data;
+            data=temp;
+        }
+        size++;
+    }
+    /**
+     * 删除一个头结点
+     * @return
+     */
+    public E deleteFirst(){
+        Node temp=data;
+        data=temp.next;
+        size--;
+        return temp.data;
+    }
+
+    /**
+     * 在index插入节点
+     * @param index
+     * @param element
+     */
+    public void add(int index,E element){
+        checkPositionIndex(index);
+        if (index==0){
+            addFirst(element);
+            return;
+        }
+        Node newNode=new Node(element,null);//新的结点
+        Node after=data;//保存index之后的结点
+        Node current=data;//保存index当前的结点
+        int i=0;//从第一个结点开始遍历
+        while (i!=index){//遍历index之前的结点
+            current=after;
+            after=after.next;
+            i++;
+        }
+        newNode.next=after;
+        current.next=newNode;
+        size++;
+    }
+
+    /**
+     * 增加一个末尾结点
+     * @param element
+     */
+    public void add(E element){
+        if (size==0){
+            addFirst(element);
+            return;
+        }
+        Node newNode=new Node(element,null);//新的结点
+        Node temp=data;
+        int j=0;
+        while (j<(size-1)){
+            temp= temp.next;//找到最后一个结点
+            j++;
+        }
+        temp.next=newNode;
+        size++;
+    }
+    /**
+     * 根据下标获取元素
+     * @param index
+     * @return
+     */
+    public E get(int index){
+       checkPositionIndex(index);
+       Node temp=data;//从头结点开始遍历
+        int j=0;
+        while(j<index){
+            temp=temp.next;
+            j++;
+        }
+        return temp.data;
+    }
+
+    /**
+     * 删除index结点
+     * @param index
+     */
+    public void delete(int index){
+        checkPositionIndex(index);
+        Node temp=data;
+        Node current=data;
+        int j=0;
+        while (j<index){
+            current=temp;
+            temp=temp.next;
+            j++;
+        }
+        if (temp==data){
+            data=data.next;
+        }else{
+            current.next=temp.next;
+        }
+        size--;
+    }
+    private void checkPositionIndex(int index) {
+        if (index<0 || index>size-1)
+            throw new IndexOutOfBoundsException("数组越界Index: "+index+", Size: "+size);
+    }
+    public int size(){
+        return size;
+    }
+
+}
--- src/main/java/com/algorithm/study/demo/LinearTable/Test.java
+++ src/main/java/com/algorithm/study/demo/LinearTable/Test.java
@@ -1,6 +1,7 @@
 package com.algorithm.study.demo.LinearTable;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -8,19 +9,41 @@ import java.util.List;
  */
 public class Test {
     public static void main(String[] args) {
-        MList<String> list=new MyArrayList<String>();
-        list.addElem(0,"hello2");
-        list.addElem(0,"hello1");
-//        list.setElem(0,"hello3");
-        list.add("hello4");
-        list.del("hello4");
-//        list.delElem(0);
-        for (int i=0;i<list.size();i++){
-            System.out.println(list.getElem(i));
+//        MList<String> list=new MyArrayList<String>();
+//        list.addElem(0,"hello2");
+//        list.addElem(0,"hello1");
+////        list.setElem(0,"hello3");
+//        list.add("hello4");
+//        list.del("hello4");
+////        list.delElem(0);
+//        for (int i=0;i<list.size();i++){
+//            System.out.println(list.getElem(i));
+//        }
+//        System.out.println(list.size());
+//        List<String> list2=new ArrayList<String>();
+//        list2.add("2");
+//        list2.remove("2");
+//
+//        LinkedList<String> list3=new LinkedList<String>();
+//        list3.add("a");
+//        list3.add("b");
+//        for (int i=0;i<list3.size();i++){
+//            System.out.println(list3.get(i));
+//        }
+
+        MLinkList<String> linkList=new MLinkList<String>();
+//        linkList.addFirst("A");
+//        linkList.addFirst("B");
+//        linkList.addFirst("C");
+//        linkList.add(2,"D");
+//        linkList.delete(3);
+        linkList.add("A");
+        linkList.add("B");
+        linkList.add("C");
+        linkList.add("D");
+//        System.out.println("已删除头结点："+linkList.deleteFirst());
+        for (int i=0;i<linkList.size();i++){
+            System.out.println(linkList.get(i));
         }
-        System.out.println(list.size());
-        List<String> list2=new ArrayList<String>();
-        list2.add("2");
-        list2.remove("2");
     }
 }
