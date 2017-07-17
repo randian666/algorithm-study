ac9e1545d0b8e42b4027e74388db8d8f5b693b36
liuxun <liuxun@jd.com>
2017-07-17 12:33:17 +0800

    JDK动态代理

--- src/main/java/com/algorithm/study/demo/reflect/ServiceHandler.java
+++ src/main/java/com/algorithm/study/demo/reflect/ServiceHandler.java
@@ -6,6 +6,9 @@ import java.lang.reflect.Proxy;
 
 /**
  * 动态代理解决方案
+ *
+ 1、需要实现InvocationHandler
+ 2、在invoke方法里面，实现动态代理， method是要被调用的方法
  * Created by liuxun on 2017/7/17.
  */
 public class ServiceHandler implements InvocationHandler {
@@ -14,6 +17,12 @@ public class ServiceHandler implements InvocationHandler {
     public ServiceHandler(Object target){
         this.target = target;
     }
+
+    /**
+     * Proxy负责生成一个代理的实例
+     * 这个实例在调用service方法的时候，改成了调用serviceHandler的invoke方法，从而把begin和end的动作插入。
+     * @return
+     */
     public Object createProxyIntance(){
         //这里调用JDK生成Proxy的地方，三个参数，类装载器，代理接口，proxy回调方法所在的对象
         return  Proxy.newProxyInstance(this.target.getClass().getClassLoader(), this.target.getClass().getInterfaces(), this);
@@ -22,8 +31,8 @@ public class ServiceHandler implements InvocationHandler {
     /**
      * 回调方法
      * @param proxy 代理对象
-     * @param method 对象方法
-     * @param args 方法参数
+     * @param method 调用的方法
+     * @param args 调用的方法参数
      * @return
      * @throws Throwable
      */
