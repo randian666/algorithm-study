7fdce5c15a6d054ad4d265418873bd2ab2681d4d
liuxun <liuxun@jd.com>
2018-03-29 10:29:06 +0800

    代理模式

--- src/main/java/com/algorithm/study/demo/mode/proxy/MainTest.java
+++ src/main/java/com/algorithm/study/demo/mode/proxy/MainTest.java
@@ -4,6 +4,7 @@ public class MainTest {
     public static void main(String[] args) {
         /**
          * 为其他对象提供一种代理以控制对这个对象的访问
+         *
          */
         IGamePlayer gamePlayer=new GamePlayer("刘勋");
         GamePlayerProxy proxy=new GamePlayerProxy(gamePlayer);
