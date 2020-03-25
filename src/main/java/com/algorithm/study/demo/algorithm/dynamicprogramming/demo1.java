b4e847701d348d7a313f3ab0befe4ea7a37b137a
刘勋 <xun2.liu@ly.com>
2020-03-25 11:33:27 +0800

    动态规划-斐波那契数列

--- src/main/java/com/algorithm/study/demo/algorithm/dynamicprogramming/demo1.java
+++ src/main/java/com/algorithm/study/demo/algorithm/dynamicprogramming/demo1.java
@@ -4,13 +4,15 @@ package com.algorithm.study.demo.algorithm.dynamicprogramming;
  * @author xun2.liu
  * @title: demo1
  * @projectName algorithm-study
- * @description: 动态规划
+ * @description: 动态规划-斐波那契数列
  * @date 2020/3/18 14:04
  */
 public class demo1 {
     public static void main(String[] args) {
         System.out.println(fib(10));
         System.out.println(fib2(10));
+        System.out.println(fib3(10));
+        System.out.println(fib4(10));
     }
 
     /**
@@ -46,5 +48,41 @@ public class demo1 {
             return memo[n];
         }
         return memo[n]=helper(memo,n-1)+helper(memo,n-2);
+    }
+
+    /**
+     * DP table 数组的迭代解法
+     * 空间复杂度降为 O(N)
+     *自底向上计算斐波那契数列
+     * @param n
+     * @return
+     */
+    private static int fib3(int n){
+        int[] temp=new int[n+1];
+        temp[1]=temp[2]=1;
+        for (int i=3;i<=n;i++){
+            temp[i]=temp[i-1]+temp[i-2];
+        }
+        return temp[n];
+    }
+
+    /**
+     * DP table 数组的迭代解法优化
+     * 空间复杂度降为 O(1)
+     *自底向上计算斐波那契数列
+     * @param n
+     * @return
+     */
+    private static int fib4(int n){
+        if (n==1 || n==2){
+            return 1;
+        }
+        int prev=1;int curr=1;
+        for (int i=3;i<=n;i++){
+            int temp=prev+curr;
+            prev=curr;
+            curr=temp;
+        }
+        return curr;
     }
 }
