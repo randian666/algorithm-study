f94d10ff337faf9e4e021f2dee2bdbfe85142bf0
liuxun <553798608@qq.com>
2020-05-24 23:45:27 +0800

    爬楼梯

--- src/main/java/com/algorithm/study/demo/algorithm/leetcode/Solution13.java
+++ src/main/java/com/algorithm/study/demo/algorithm/leetcode/Solution13.java
@@ -41,12 +41,15 @@ public class Solution13 {
         if (n<=0){
             return 0;
         }
+        if (n==1){
+            return 1;
+        }
+        if (n==2){
+            return 2;
+        }
         int[] dp=new int[n+1];
         dp[1]=1;
         dp[2]=2;
-        if (n<=2){
-            return dp[n];
-        }
         for (int i = 3; i <=n; i++) {
             dp[i]=dp[i-1]+dp[i-2];
         }
@@ -54,6 +57,6 @@ public class Solution13 {
     }
 
     public static void main(String[] args) {
-        System.out.println(climbStairs(10));
+        System.out.println(climbStairs(1));
     }
 }
